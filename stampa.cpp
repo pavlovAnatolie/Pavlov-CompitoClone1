2bdedaa92c0a6aa493cc80106e0790067a6c5b18
pavlovAnatolie <lai2-01@itismeucci.com>
2023-03-30 14:25:28 +0200

    inserito nome

--- stampa.cpp
+++ stampa.cpp
@@ -1,7 +1,7 @@
 /*
 Scrivere un programma che stampi "n" volte la stringa "Bada come la fuma"  con "n" dato in input dall'utente.
 
-NOME COGNOME DATA
+Anatolie Pavlov 30/03/23
 */
 
 # include <iostream>
